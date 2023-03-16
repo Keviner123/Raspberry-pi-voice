a9795381e4db205dfd26c71ff98079c66b47c8a7
Kevin <kevin@kevinfrost.dk>
2023-03-16 08:17:32 +0100

    .

--- wifi/run.py
+++ wifi/run.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template, request
 import subprocess
 import time
 
+def set_wifi_connection():
+
+    # Generate the wpa_supplicant.conf file content
+    wpa_supplicant_conf = '''
+    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+    update_config=1
+
+    network={
+            ssid="internet"
+            psk="Kode123!"
+    }
+    '''
+
+    # Write the wpa_supplicant.conf file to disk
+    with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as f:
+        f.write(wpa_supplicant_conf)
+
+    time.sleep(5)
+
+    subprocess.run(['sudo reboot'])
+
+    time.sleep(5)
 
 app = Flask(__name__)
 
@@ -11,6 +33,7 @@ def index():
         username = request.form['username']
         password = request.form['password']
         print(f'Username: {username}, Password: {password}')
+        set_wifi_connection()
     return render_template('index.html')
 
 
@@ -22,5 +45,4 @@ if __name__ == '__main__':
     time.sleep(5)
 
     app.run(host="0.0.0.0", port="80")
-
     create_ap_process.terminate()
