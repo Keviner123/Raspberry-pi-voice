1c1cfe1c9dfc037c75df7176772435aa9e9653ff
Kevin <kevin@kevinfrost.dk>
2023-03-17 12:10:07 +0100

    .

--- Controller/SoundFilePlayer.py
+++ Controller/SoundFilePlayer.py
@@ -1,17 +1,18 @@
+import threading
 import pygame
 
 class SoundFilePlayer:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        pygame.mixer.init()
 
     def PlayMp3(self, filelocation: str):
+        def play_mp3():
+            pygame.mixer.music.load(filelocation)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+            pygame.mixer.quit()
 
-        pygame.mixer.init()
-        pygame.mixer.music.load(filelocation)
-        pygame.mixer.music.play()
-
-        while pygame.mixer.music.get_busy():
-            pygame.time.Clock().tick(10)  # wait for the music to finish playing
-
-        pygame.quit()
+        thread = threading.Thread(target=play_mp3)
+        thread.start()
