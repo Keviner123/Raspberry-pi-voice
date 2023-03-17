7a8f0cc437a8ad119d3d5ef6398fcd5c6e2ba9f9
Kevin <kevin@kevinfrost.dk>
2023-03-17 11:02:55 +0100

    Update VoiceActivation.py

--- Controller/VoiceActivation.py
+++ Controller/VoiceActivation.py
@@ -1,8 +1,12 @@
+import audioop
+import math
 import threading
 import time
 import pvporcupine
 import pyaudio
 import struct
+
+import pygame
 
 from Controller.SoundFilePlayer import SoundFilePlayer
 
@@ -12,7 +16,66 @@ class VoiceActivation:
         self.activationSound = activationSound
     
     def Answer(self):
-        time.sleep(30)
+
+        pygame.mixer.init()
+        sound = pygame.mixer.Sound('assets/ding.wav')
+        playing = sound.play()
+
+
+        # Set the silence threshold value (in dB)
+        THRESHOLD = 60
+
+        # Set the chunk size and recording duration
+        CHUNK_SIZE = 1024
+        RECORD_DURATION = 10  # in seconds
+
+        # Set the PyAudio parameters
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 44100
+
+        # Create the PyAudio object
+        audio = pyaudio.PyAudio()
+
+        # Open the default input device for recording
+        stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+
+        # Initialize variables for the output file and the silence counter
+        output_file = wave.open('output.wav', 'wb')
+        output_file.setnchannels(1)
+        output_file.setsampwidth(audio.get_sample_size(FORMAT))
+        output_file.setframerate(RATE)
+
+        silence_counter = 0
+
+        # Record audio until there is silence
+        while silence_counter < 50:  # adjust this value to set the minimum silence duration in frames
+            # Read a chunk of audio data from the input stream
+            data = stream.read(CHUNK_SIZE)
+
+            # Compute the RMS amplitude of the audio data (in dB)
+            rms = audioop.rms(data, 2)
+            db = 20 * math.log10(rms) if rms > 0 else 0
+
+            # Write the audio data to the output file
+            output_file.writeframes(data)
+
+            # Check if the audio is silent
+            if db < THRESHOLD:
+                silence_counter += 1
+            else:
+                silence_counter = 0
+
+            # Log the dB value
+            print(f"dB: {db:.2f}")
+
+
+        # Close the output file and the input stream
+        output_file.close()
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
 
     def StartListening(self):
 
