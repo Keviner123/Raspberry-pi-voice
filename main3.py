77e06aaf92d070f77801d7df0e9132f1804be5db
My Name <my-name@chromium.org>
2023-03-19 12:49:02 +0100

    s

--- main3.py
+++ main3.py
@@ -1,14 +1,15 @@
 
-# from Controller.SoundFilePlayer import SoundFilePlayer
 # from Controller.Webserver import Webserver
-from View.hotword_detector import HotwordDetector
-
+from View.hotword_listener import HotwordDetector
+from DAL.SoundFilePlayer import SoundFilePlayer
 
 # WebserverController = Webserver(80)
+soundfileplayer = SoundFilePlayer()
 
 
 if __name__ == '__main__':
-    hotwordetector = HotwordDetector("assets/ding.mp3", "KqwUDxJhP+vf3BYnrH3/VXb5Uy2qOr50MhrMCflhbybizGB15keeeA==")
+    hotwordetector = HotwordDetector("KqwUDxJhP+vf3BYnrH3/VXb5Uy2qOr50MhrMCflhbybizGB15keeeA==")
 
     while hotwordetector.wait_for_hotwords():
         print("hej")
+        soundfileplayer.PlayMp3("assets/ding.mp3")
