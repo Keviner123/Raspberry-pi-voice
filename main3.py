9b2984d5b58e1ad2c92d5599a65a5e9b912cfac4
My Name <my-name@chromium.org>
2023-03-19 14:05:13 +0100

    .

--- main3.py
+++ main3.py
@@ -6,9 +6,7 @@ from View.voice_listener import VoiceListener
 from DAL.sound_file_player import SoundFilePlayer
 
 
-
 if __name__ == '__main__':
-
     with open("config.yaml", "r", encoding="utf-8") as yamlfile:
         config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
@@ -17,14 +15,14 @@ if __name__ == '__main__':
     voicelistener = VoiceListener()
     texttospeechconverter = TextToSpeechConverter()
 
-
     while hotwordetector.wait_for_hotwords():
         soundfileplayer.play_mp3_async(config["activation-sound"])
-
         voicelistener.start_recording()
 
-        TranscribeText = voicelistener.transcribe()
+        try:
+            TranscribeText = voicelistener.transcribe()
+            texttospeechconverter.convert_text_to_mp3(TranscribeText)
+            soundfileplayer.play_mp3_async("output.mp3")
 
-        texttospeechconverter.convert_text_to_mp3(TranscribeText)
-
-        soundfileplayer.play_mp3_async("output.mp3")
+        except IndexError:
+            print("No voice detected")
