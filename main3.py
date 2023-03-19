27260625d6e64a8b9a6d9b7a3a5cef6bbdaa00bf
My Name <my-name@chromium.org>
2023-03-19 14:35:27 +0100

    .

--- main3.py
+++ main3.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
 
         try:
             transcribe_text = voicelistener.transcribe()
+            print("transcribe_text")
+
             question_answer = questionansweringservice.get_answer(transcribe_text)
             texttospeechconverter.convert_text_to_mp3(question_answer)
 
